d76215c58128f6bd0c92e8c41ed34d1604a1db21
Soulbolt <soulbolt@gmail.com>
2024-02-02 22:37:55 -0700

    add FriendList and Friend components. Iterate through the friend list object, Use Friend component to define the list to display images, names, and context of balances using conditionals and styling for clarity.

--- src/App.js
+++ src/App.js
@@ -20,5 +20,44 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hi</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}.
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}.
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
+    </li>
+  );
 }
